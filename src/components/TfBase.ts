575ab3a1ea13d9edb9c2cb5f96dd9c2dc5c46ae5
TopNiz <nizar.ayed@upgrade-code.org>
2023-07-04 20:02:26 +0200

    Fixed theming system

--- src/components/TfBase.ts
+++ src/components/TfBase.ts
@@ -138,29 +138,29 @@ const style = css`
       line-height: 2rem;
    }
 
-   * {
-      --tf-sys-light-primary: var(--tf-sys-light-primary, #00aae3);
-      --tf-sys-light-onprimary: var(--tf-sys-light-onprimary, #250127);
-      --tf-sys-light-primary-container: var(--tf-sys-light-primary-container, #c2e8ff);
-      --tf-sys-light-secondary: var(--tf-sys-light-secondary, #ff805e);
-      --tf-sys-light-onsecondary: var(--tf-sys-light-onsecondary, #250127);
-      --tf-sys-light-secondary-container: var(--tf-sys-light-secondary-container, #ffdacf);
-      --tf-sys-light-tertiary: var(--tf-sys-light-tertiary, #ffb030);
-      --tf-sys-light-ontertiary: var(--tf-sys-light-ontertiary, #250127);
-      --tf-sys-light-tertiary-container: var(--tf-sys-light-tertiary-container, #ffddb0);
-      --tf-sys-light-error: var(--tf-sys-light-error, #ba1b1b);
-      --tf-sys-light-on-error: var(--tf-sys-light-on-error, #ffffff);
-      --tf-sys-light-error-container: var(--tf-sys-light-error-container, #ffdad4);
-      --tf-sys-light-surface-variant: var(--tf-sys-light-surface-variant, #d4d4d4);
-      --tf-sys-light-surface: var(--tf-sys-light-surface, #f9f9f8);
-      --tf-sys-light-outline: var(--tf-sys-light-outline, #71787d);
-      --tf-sys-light-background: var(--tf-sys-light-background, #f3f3f3);
+   :host {
+      --tf-sys-light-primary: var(--light-primary, #00aae3);
+      --tf-sys-light-onprimary: var(--light-onprimary, #250127);
+      --tf-sys-light-primary-container: var(--light-primary-container, #c2e8ff);
+      --tf-sys-light-secondary: var(--light-secondary, #ff805e);
+      --tf-sys-light-onsecondary: var(--light-onsecondary, #250127);
+      --tf-sys-light-secondary-container: var(--light-secondary-container, #ffdacf);
+      --tf-sys-light-tertiary: var(--light-tertiary, #ffb030);
+      --tf-sys-light-ontertiary: var(--light-ontertiary, #250127);
+      --tf-sys-light-tertiary-container: var(--light-tertiary-container, #ffddb0);
+      --tf-sys-light-error: var(--light-error, #ba1b1b);
+      --tf-sys-light-on-error: var(--light-on-error, #ffffff);
+      --tf-sys-light-error-container: var(--light-error-container, #ffdad4);
+      --tf-sys-light-surface-variant: var(--light-surface-variant, #d4d4d4);
+      --tf-sys-light-surface: var(--light-surface, #f9f9f8);
+      --tf-sys-light-outline: var(--light-outline, #71787d);
+      --tf-sys-light-background: var(--light-background, #f3f3f3);
 
-      --tf-subhead1: var(--tf-subhead1, 700 1rem/2rem Nunito, sans-serif);
-      --tf-caption: var(--tf-caption, 400 0.75rem/1rem Nunito, sans-serif);
-      --tf-body-small: var(--tf-body-small, 400 0.75rem/1rem Nunito, sans-serif);
-      --tf-button: var(--tf-button, 700 1rem Nunito, sans-serif);
-      --tf-body1: var(--tf-body1, 400 1rem/1.5rem Nunito, sans-serif);
+      --tf-subhead1: var(--subhead1, 700 1rem/2rem Nunito, sans-serif);
+      --tf-caption: var(--caption, 400 0.75rem/1rem Nunito, sans-serif);
+      --tf-body-small: var(--body-small, 400 0.75rem/1rem Nunito, sans-serif);
+      --tf-button: var(--button, 700 1rem Nunito, sans-serif);
+      --tf-body1: var(--body1, 400 1rem/1.5rem Nunito, sans-serif);
    }
 
    .primary {
