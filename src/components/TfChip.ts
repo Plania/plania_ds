4bcdc907ba03a5ed337bf8d895adc6afbac91bcc
linabarkaoui <lina.barkaoui3@gmail.com>
2023-07-13 17:06:30 +0100

    Fix #18

--- src/components/TfChip.ts
+++ src/components/TfChip.ts
@@ -1,83 +1,100 @@
 import { css, html, TfBase } from './TfBase.js';
 
 const style = css`
-   span {
-      background-color: var(--tf-sys-light-surface);
-      outline: 1px solid var(--tf-sys-light-primary);
-      border-radius: 1rem;
-      padding: 0.25rem 0.5rem;
-      font: var(--tf-body-small);
-      display: flex;
-      align-items: center;
-      width: fit-content;
-      cursor: pointer;
-   }
+  span {
+    background-color: var(--tf-sys-light-surface);
+    outline: 1px solid var(--tf-sys-light-primary);
+    border-radius: 1rem;
+    padding: 0.25rem 0.5rem;
+    font: var(--tf-body-small);
+    display: flex;
+    align-items: center;
+    width: fit-content;
+    cursor: pointer;
+  }
 
-   span > tf-icon {
-      width: 1rem;
-      height: 1rem;
-      color: var(--tf-sys-light-onprimary);
-   }
+  span > tf-icon {
+    width: 1rem;
+    height: 1rem;
+    color: var(--tf-sys-light-onprimary);
+  }
 
-   .selected {
-      background-color: var(--tf-sys-light-primary);
-   }
+  .selected {
+  }
+  .primary {
+    outline-color: var(--tf-sys-light-primary);
+  }
+  .secondary {
+    outline-color: var(--tf-sys-light-secondary);
+  }
+  .tertiary {
+    outline-color: var(--tf-sys-light-tertiary);
+  }
 
-   .disabled {
-      color:var(--tf-sys-light-outline);
-      background-color: var(--tf-sys-light-surface-variant);
-      outline-color: var(--tf-sys-light-outline);
-      cursor: default;
-   }
+  .not-selected {
+    background: var(--tf-sys-light-surface);
+  }
 
-   .chip-card {
-      background-color: var(--tf-sys-light-surface);
-      outline:none;
-      box-shadow: 2px 2px 4px 0px #00000040;
-      padding: 0rem 0.5rem;
-   }
+  .disabled {
+    color: var(--tf-sys-light-outline);
+    background-color: var(--tf-sys-light-surface-variant);
+    outline-color: var(--tf-sys-light-outline);
+    cursor: default;
+  }
+
+  .chip-card {
+    background-color: var(--tf-sys-light-surface);
+    outline: none;
+    box-shadow: 2px 2px 4px 0px #00000040;
+    padding: 0rem 0.5rem;
+  }
 `;
 
 export class TfChip extends TfBase {
   constructor() {
     super();
     this.shadowRoot &&
-         (this.shadowRoot.innerHTML += html`
-            <style>
-               ${style}
-            </style>
-            <span class='disabled'>
-               <slot></slot>
-            </span>
-         `);
+      (this.shadowRoot.innerHTML += html`
+        <style>
+          ${style}
+        </style>
+        <span class="disabled">
+          <slot></slot>
+        </span>
+      `);
   }
 
   // connectedCallback() {}
 
   static get observedAttributes() {
-    return ['selected', 'active', 'symbol', 'icon'];
+    return ['selected', 'active', 'symbol', 'icon', 'variant'];
   }
 
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
     const chipElem = this.shadowRoot?.querySelector('span');
     if (!chipElem) return;
-    chipElem.classList.remove('selected');
     chipElem.classList.remove('disabled');
 
-    if(!this.active){
+    if (!this.active) {
       chipElem.classList.add('disabled');
     }
+    if (!this.selected) {
+      chipElem.classList.remove('selected');
+      chipElem.classList.add('not-selected');
+    }
     switch (name) {
-    case 'selected':
-      chipElem.classList.add('selected');
-      break;
-    case 'symbol':
-      if (!this.icon) return;
-      chipElem.insertAdjacentHTML(
-        'afterbegin',
-        '<tf-icon icon="' + newValue + '"></tf-icon>'
-      );
-      break;
+      case 'selected':
+        chipElem.classList.remove('not-selected');
+        chipElem.classList.add('selected');
+        break;
+      case 'symbol':
+        if (!this.icon) return;
+        chipElem.insertAdjacentHTML('afterbegin', '<tf-icon icon="' + newValue + '"></tf-icon>');
+      case 'variant':
+        chipElem.classList.remove(oldValue);
+        chipElem.classList.add(newValue);
+
+        break;
     }
   }
 
@@ -123,12 +140,20 @@ export class TfChip extends TfBase {
     value && this.setAttribute('icon', '');
     !value && this.removeAttribute('icon');
   }
+
+  get variant() {
+    return this.getAttribute('variant');
+  }
+
+  set variant(value) {
+    this.setAttribute('variant', value ?? 'primary');
+  }
 }
 
 declare global {
-   interface HTMLElementTagNameMap {
-      'tf-chip': TfChip;
-   }
+  interface HTMLElementTagNameMap {
+    'tf-chip': TfChip;
+  }
 }
 
 customElements.define('tf-chip', TfChip);
