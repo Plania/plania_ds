e876dcf6d1214bf6e722d7e936776440354a11e2
linabarkaoui <lina.barkaoui3@gmail.com>
2023-07-14 11:07:41 +0100

    Fix #35

--- src/stylebook/StyleTfTextButton.ts
+++ src/stylebook/StyleTfTextButton.ts
@@ -7,7 +7,7 @@ const textButton: StyleVariantProps<'tf-text-button'> = {
   tag: 'tf-text-button',
   description: 'A text button.',
   data: {
-    content: 'Text Button',
+    content: ' Skip',
   },
 };
 
@@ -16,8 +16,8 @@ const textButtonPrefixIcon: StyleVariantProps<'tf-text-button'> = {
   tag: 'tf-text-button',
   description: 'A text button with prefix icon.',
   data: {
-    content: 'Text Button',
-    'prefix-icon': '<tf-icon icon=\'arrow-forward-ios\'></tf-icon>',
+    content: 'Skip',
+    'prefix-icon': '<tf-icon icon=\'arrow-back-ios\'></tf-icon>',
   },
 };
 
@@ -26,8 +26,8 @@ const textButtonSuffixIcon: StyleVariantProps<'tf-text-button'> = {
   tag: 'tf-text-button',
   description: 'A text button with suffix icon.',
   data: {
-    content: 'Text Button',
-    'suffix-icon': 'icon',
+    content: 'Skip',
+    'suffix-icon': '<tf-icon icon=\'arrow-forward-ios\'></tf-icon>',
   },
 };
 
