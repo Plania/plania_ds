46734ac9c024ff5d4fb2f3034b84b1b1307ce9be
NourSafta <nour.safta@medtech.tn>
2023-07-15 14:56:42 +0100

    Dropdown Sub-Component Created - Fix #19

---
+++ src/components/TfDropdownItem.ts
@@ -0,0 +1,42 @@
+import { css, html, TfBase } from './TfBase.js';
+
+const style = css`
+
+   .container {
+      display: flex;
+      align-items: flex-start;
+      justify-content: flex-start;
+   }
+
+   .text {
+      color: var(--tf-sys-light-on-primary);
+      font: var(--tf-body-medium);
+   }
+`;
+
+export class TfDropdownItem extends TfBase {
+  constructor() {
+    super();
+
+    this.shadowRoot &&
+         (this.shadowRoot.innerHTML += html`
+            <style>
+               ${style}
+            </style>
+               <div class="container">
+                  <p class="text">
+                     <slot></slot>
+                  </p>
+               </div>
+         `);
+  }
+
+}
+
+declare global {
+   interface HTMLElementTagNameMap {
+      'tf-dropdown-item': TfDropdownItem;
+   }
+}
+
+customElements.define('tf-dropdown-item', TfDropdownItem);
