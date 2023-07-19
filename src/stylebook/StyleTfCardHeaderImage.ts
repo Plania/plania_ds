a36129e2890b7a2cf896eb1dcddbb6372cf05b63
Nathan <coursnathan57@gmail.com>
2023-07-19 15:47:53 +0200

    Fix #48

--- src/stylebook/StyleTfCardHeaderImage.ts
+++ src/stylebook/StyleTfCardHeaderImage.ts
@@ -3,11 +3,43 @@ import { StyleComponentProps } from './StyleComponent.js';
 import { StyleVariantProps } from './StyleVariant.js';
 
 const defaultCardHeaderImage: StyleVariantProps<'tf-card-header-image'> = {
-  name: 'Default Card Header Image',
+  name: 'Card Header image',
   tag: 'tf-card-header-image',
-  description: '',
+  description: 'Card header image without eco badge and not in favorite',
   data: {
     src: '/assets/image.png',
+    eco : 'none',
+  },
+};
+
+const ecoNotFavorite: StyleVariantProps<'tf-card-header-image'> = {
+  name: 'Card Header image',
+  tag: 'tf-card-header-image',
+  description: 'Card header image with eco badge and not in favorite',
+  data: {
+    src: '/assets/image.png',
+    badge : 'eco',
+  },
+};
+
+const noEcoFavorite: StyleVariantProps<'tf-card-header-image'> = {
+  name: 'Card Header image',
+  tag: 'tf-card-header-image',
+  description: 'Card header image without eco badge and in favorite',
+  data: {
+    src: '/assets/image.png',
+    favorite : '',
+  },
+};
+
+const ecoFavorite: StyleVariantProps<'tf-card-header-image'> = {
+  name: 'Card Header image',
+  tag: 'tf-card-header-image',
+  description: 'Card header image with eco badge and in favorite',
+  data: {
+    src: '/assets/image.png',
+    badge : 'eco',
+    favorite : '',
   },
 };
 
@@ -16,7 +48,12 @@ const meta: StyleComponentProps<'tf-card-header-image'> = {
   tag: 'tf-card-header-image',
   description: 'Tourisfair card header image component, showing the header image of a card.',
   component: 'Tourisfair Card Header Image',
-  variants: [defaultCardHeaderImage],
+  variants: [
+    defaultCardHeaderImage,
+    ecoNotFavorite,
+    noEcoFavorite,
+    ecoFavorite
+  ],
 };
 
 export const styleTfCardHeaderImage = (styleBook: StyleBook) => styleBook.addComponent(meta);
