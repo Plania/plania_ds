7e274b8d70b06cc238d29f327adcd19e2aa8d24c
stuprog <sami.chakroun@medtech.tn>
2023-08-04 14:01:15 +0100

    Create tf-step -Fix #26

--- src/components/TfStep.ts
+++ src/components/TfStep.ts
@@ -1,53 +1,55 @@
 import { css, html, TfBase } from './TfBase.js';
 
 export class TfStep extends TfBase {
-    static get observedAttributes(): string[] {
-        return ['step', 'variant'];
+  static get observedAttributes(): string[] {
+    return ['step', 'variant'];
+  }
+
+  constructor() {
+    super();
+
+    if (!this.shadowRoot) {
+      this.attachShadow({ mode: 'open' });
+      this.render();
     }
+  }
 
-    constructor() {
-        super();
-
-        if (!this.shadowRoot) {
-            this.attachShadow({ mode: 'open' });
-            this.render();
-        }
+  attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null) {
+    if (name === 'step' || name === 'variant') {
+      this.render();
     }
+  }
 
-    attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null) {
-        if (name === 'step' || name === 'variant') {
-            this.render();
-        }
-    }
+  render() {
+    const step = parseInt(this.getAttribute('step') || '1', 10);
+    const variant = this.getAttribute('variant') || 'not-selected';
 
-    render() {
-        const step = parseInt(this.getAttribute('step') || '1', 10);
-        const variant = this.getAttribute('variant') || 'not-selected';
+    const style = css`
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0.25rem 0.25rem;
+        width: 1.5rem;
+        height: 1.5rem;
+        border-radius: 100%;
+        background: ${variant === 'selected'
+          ? 'var(--light-tertiary)'
+          : 'var(--light-surface-variant)'};
+      }
+    `;
 
-        const style = css`
-            .container {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                padding: 4px 4px;
-                width: 24px;
-                height: 24px;
-                border-radius: 15px;
-                background: ${variant === 'selected' ? 'var(--light-tertiary)' : 'var(--light-surface-variant)'};
-            }
-        `;
-
-        if (this.shadowRoot) {
-            this.shadowRoot.innerHTML = `
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = `
                 <style>${style}</style>
                 <div class="container">${step}</div>
             `;
-        }
     }
+  }
 }
 declare global {
-           interface HTMLElementTagNameMap {
-              'tf-step': TfStep;
-           }
-        }
+  interface HTMLElementTagNameMap {
+    'tf-step': TfStep;
+  }
+}
 customElements.define('tf-step', TfStep);
