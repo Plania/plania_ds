eb47c605ddd7e253b7bd01a07993967ef994e9af
NourSafta <nour.safta@medtech.tn>
2023-08-20 12:34:07 +0100

    StyleBook entry for Icons Created - Fix #64

--- src/stylebook/StyleTfIcon.ts
+++ src/stylebook/StyleTfIcon.ts
@@ -2,21 +2,12 @@ import { StyleBook } from './StyleBook.js';
 import { StyleComponentProps } from './StyleComponent.js';
 import { StyleVariantProps } from './StyleVariant.js';
 
-const arrowForwardIosIcon: StyleVariantProps<'tf-icon'> = {
-  name: 'Arrow forward ios icon',
+const accountCircleIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Account Circle icon',
   tag: 'tf-icon',
-  description: 'Arrow forward ios icon',
+  description: 'Account Circle icon',
   data: {
-    icon: 'arrow-forward-ios',
-  },
-};
-
-const arrowBackIosIcon: StyleVariantProps<'tf-icon'> = {
-  name: 'Arrow back ios icon',
-  tag: 'tf-icon',
-  description: 'Arrow back ios icon',
-  data: {
-    icon: 'arrow-back-ios',
+    icon: 'account-circle',
   },
 };
 
@@ -29,12 +20,404 @@ const addIcon: StyleVariantProps<'tf-icon'> = {
   },
 };
 
+const arrowBackIosIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Arrow back ios icon',
+  tag: 'tf-icon',
+  description: 'Arrow back ios icon',
+  data: {
+    icon: 'arrow-back-ios',
+  },
+};
+
+const arrowDropdDownIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Arrow dropdown icon',
+  tag: 'tf-icon',
+  description: 'Arrow dropdown icon',
+  data: {
+    icon: 'arrow-drop-down',
+  },
+};
+
+const arrowDropdUpIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Arrow drop up icon',
+  tag: 'tf-icon',
+  description: 'Arrow drop up icon',
+  data: {
+    icon: 'arrow-drop-up',
+  },
+};
+
+const arrowForwardIosIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Arrow forward ios icon',
+  tag: 'tf-icon',
+  description: 'Arrow forward ios icon',
+  data: {
+    icon: 'arrow-forward-ios',
+  },
+};
+
+const changeCircleIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Change circle icon',
+  tag: 'tf-icon',
+  description: 'Change circle icon',
+  data: {
+    icon: 'change-circle',
+  },
+};
+
+const checkIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Check icon',
+  tag: 'tf-icon',
+  description: 'Check icon',
+  data: {
+    icon: 'check',
+  },
+};
+
+const chevronLeftIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Chevron Left icon',
+  tag: 'tf-icon',
+  description: 'Chevron Left icon',
+  data: {
+    icon: 'chevron-left',
+  },
+};
+
+const chevronRightIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Chevron Right icon',
+  tag: 'tf-icon',
+  description: 'Chevron Right icon',
+  data: {
+    icon: 'chevron-right',
+  },
+};
+
+const childFriendlyIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Child Friendly icon',
+  tag: 'tf-icon',
+  description: 'Child Friendly icon',
+  data: {
+    icon: 'child-friendly',
+  },
+};
+
+const dateRangeIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Date Range icon',
+  tag: 'tf-icon',
+  description: 'Date Range icon',
+  data: {
+    icon: 'date-range',
+  },
+};
+
+const euroSymbolIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Euro Symbol icon',
+  tag: 'tf-icon',
+  description: 'Euro Symbol icon',
+  data: {
+    icon: 'euro-symbol',
+  },
+};
+
+const exploreIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Explore icon',
+  tag: 'tf-icon',
+  description: 'Explore icon',
+  data: {
+    icon: 'explore',
+  },
+};
+
+const eyesIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Explore icon',
+  tag: 'tf-icon',
+  description: 'Eyes icon',
+  data: {
+    icon: 'eyes',
+  },
+};
+
+const favoriteIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Favorite icon',
+  tag: 'tf-icon',
+  description: 'Favorite icon',
+  data: {
+    icon: 'favorite',
+  },
+};
+
+const favoriteBorderIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Favorite Border icon',
+  tag: 'tf-icon',
+  description: 'Favorite Border icon',
+  data: {
+    icon: 'favorite-border',
+  },
+};
+
+const halfStarIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Half Star icon',
+  tag: 'tf-icon',
+  description: 'Half Star icon',
+  data: {
+    icon: 'half-star',
+  },
+};
+
+const hourGlassBottomIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Hour Glass Bottom icon',
+  tag: 'tf-icon',
+  description: 'Hour Glass Bottom icon',
+  data: {
+    icon: 'hourglass-bottom',
+  },
+};
+
+const hourGlassTopIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Hour Glass Top icon',
+  tag: 'tf-icon',
+  description: 'Hour Glass Top icon',
+  data: {
+    icon: 'hourglass-top',
+  },
+};
+
+const keyboardArrowDownIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Keyboard Arrow Down icon',
+  tag: 'tf-icon',
+  description: 'Keyboard Arrow Down icon',
+  data: {
+    icon: 'keyboard-arrow-down',
+  },
+};
+
+const keyboardArrowRightIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Keyboard Arrow Right icon',
+  tag: 'tf-icon',
+  description: 'Keyboard Arrow Right icon',
+  data: {
+    icon: 'keyboard-arrow-right',
+  },
+};
+
+const languageIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Language icon',
+  tag: 'tf-icon',
+  description: 'Language icon',
+  data: {
+    icon: 'language',
+  },
+};
+
+const locationCityIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Location City icon',
+  tag: 'tf-icon',
+  description: 'Location City  icon',
+  data: {
+    icon: 'location-city',
+  },
+};
+
+const locationOnIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Location On icon',
+  tag: 'tf-icon',
+  description: 'Location On  icon',
+  data: {
+    icon: 'location-on',
+  },
+};
+
+const lockIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Lock icon',
+  tag: 'tf-icon',
+  description: 'Lock icon',
+  data: {
+    icon: 'lock',
+  },
+};
+
+const lowPriorityIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Low Priority icon',
+  tag: 'tf-icon',
+  description: 'Low Priority  icon',
+  data: {
+    icon: 'low-priority',
+  },
+};
+
+const manIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Man icon',
+  tag: 'tf-icon',
+  description: 'Man icon',
+  data: {
+    icon: 'man',
+  },
+};
+
+const messageIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Message icon',
+  tag: 'tf-icon',
+  description: 'Message icon',
+  data: {
+    icon: 'message',
+  },
+};
+
+const numbersIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Numbers icon',
+  tag: 'tf-icon',
+  description: 'Numbers icon',
+  data: {
+    icon: 'numbers',
+  },
+};
+
+const phoneIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Phone icon',
+  tag: 'tf-icon',
+  description: 'Phone icon',
+  data: {
+    icon: 'phone',
+  },
+};
+
+const poundSymbolIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Pound Symbol icon',
+  tag: 'tf-icon',
+  description: 'Pound Symbol icon',
+  data: {
+    icon: 'pound-symbol',
+  },
+};
+
+const removeIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Remove icon',
+  tag: 'tf-icon',
+  description: 'Remove icon',
+  data: {
+    icon: 'remove',
+  },
+};
+
+const searchIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Search icon',
+  tag: 'tf-icon',
+  description: 'Search icon',
+  data: {
+    icon: 'search',
+  },
+};
+
+const sendIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Send icon',
+  tag: 'tf-icon',
+  description: 'Send icon',
+  data: {
+    icon: 'send',
+  },
+};
+
+const starOutlinedIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Star Outlined icon',
+  tag: 'tf-icon',
+  description: 'Star Outlined icon',
+  data: {
+    icon: 'star-outlined',
+  },
+};
+
+const starRateIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Star Rate icon',
+  tag: 'tf-icon',
+  description: 'Star Rate icon',
+  data: {
+    icon: 'star-rate',
+  },
+};
+
+const trashIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Trash icon',
+  tag: 'tf-icon',
+  description: 'Trash icon',
+  data: {
+    icon: 'trash',
+  },
+};
+
+const visibilityIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Visibility icon',
+  tag: 'tf-icon',
+  description: 'Visibility icon',
+  data: {
+    icon: 'visibility',
+  },
+};
+
+const visibilityOffIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'Visibility Off icon',
+  tag: 'tf-icon',
+  description: 'Visibility Off icon',
+  data: {
+    icon: 'visibility-off',
+  },
+};
+
+const worldIcon: StyleVariantProps<'tf-icon'> = {
+  name: 'World icon',
+  tag: 'tf-icon',
+  description: 'World icon',
+  data: {
+    icon: 'world',
+  },
+};
 const meta: StyleComponentProps<'tf-icon'> = {
   ref: 'tf-icon',
   tag: 'tf-icon',
   description: 'Tourisfair icon component. It is used to showcase an icon',
   component: 'Tourisfair Icon',
-  variants: [arrowForwardIosIcon, arrowBackIosIcon, addIcon],
+  variants: [
+    accountCircleIcon,
+    addIcon,
+    arrowBackIosIcon,
+    arrowDropdDownIcon,
+    arrowDropdUpIcon,
+    arrowForwardIosIcon,
+    changeCircleIcon,
+    checkIcon,
+    chevronLeftIcon,
+    chevronRightIcon,
+    childFriendlyIcon,
+    dateRangeIcon,
+    euroSymbolIcon,
+    exploreIcon,
+    eyesIcon,
+    favoriteIcon,
+    favoriteBorderIcon,
+    halfStarIcon,
+    hourGlassBottomIcon,
+    hourGlassTopIcon,
+    keyboardArrowDownIcon,
+    keyboardArrowRightIcon,
+    languageIcon,
+    locationCityIcon,
+    locationOnIcon,
+    lockIcon,
+    lowPriorityIcon,
+    manIcon,
+    messageIcon,
+    numbersIcon,
+    phoneIcon,
+    poundSymbolIcon,
+    removeIcon,
+    searchIcon,
+    sendIcon,
+    starOutlinedIcon,
+    starRateIcon,
+    trashIcon,
+    visibilityIcon,
+    visibilityOffIcon,
+    worldIcon,
+  ],
 };
 
 export const styleTfIcon = (styleBook: StyleBook) => styleBook.addComponent(meta);
