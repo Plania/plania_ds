25f446c741b6a8d7b03e652943cd7c4d5bd36316
TopNiz <nizar.ayed@upgrade-code.org>
2023-08-28 13:52:30 +0200

    Correctly implemented stepper

--- src/stylebook/StyleTfStepper.ts
+++ src/stylebook/StyleTfStepper.ts
@@ -2,49 +2,64 @@ import { StyleBook } from './StyleBook.js';
 import { StyleComponentProps } from './StyleComponent.js';
 import { StyleVariantProps } from './StyleVariant.js';
 
-const variant1: StyleVariantProps<'tf-stepper'> = {
+const step1: StyleVariantProps<'tf-stepper'> = {
   name: 'Stepper at Step 1',
   tag: 'tf-stepper',
   description: 'Indicates that we are on step 1',
   data: {
-    variant: '1'
-  }
+    steps: '5',
+    current: '1',
+  },
 };
 
-const variant2: StyleVariantProps<'tf-stepper'> = {
+const step2: StyleVariantProps<'tf-stepper'> = {
   name: 'Stepper at Step 2',
   tag: 'tf-stepper',
   description: 'Indicates that we are on step 2',
   data: {
-    variant: '2'
-  }
+    steps: '5',
+    current: '2',
+  },
 };
 
-const variant3: StyleVariantProps<'tf-stepper'> = {
+const step3: StyleVariantProps<'tf-stepper'> = {
   name: 'Stepper at Step 3',
   tag: 'tf-stepper',
   description: 'Indicates that we are on step 3',
   data: {
-    variant: '3'
-  }
+    steps: '5',
+    current: '3',
+  },
 };
 
-const variant4: StyleVariantProps<'tf-stepper'> = {
+const step4: StyleVariantProps<'tf-stepper'> = {
   name: 'Stepper at Step 4',
   tag: 'tf-stepper',
   description: 'Indicates that we are on step 4',
   data: {
-    variant: '4'
-  }
+    steps: '5',
+    current: '4',
+  },
 };
 
-const variant5: StyleVariantProps<'tf-stepper'> = {
+const step5: StyleVariantProps<'tf-stepper'> = {
   name: 'Stepper at Step 5',
   tag: 'tf-stepper',
   description: 'Indicates that we are on step 5',
   data: {
-    variant: '5'
-  }
+    steps: '5',
+    current: '5',
+  },
+};
+
+const step73: StyleVariantProps<'tf-stepper'> = {
+  name: '7 steps Stepper at Step 3',
+  tag: 'tf-stepper',
+  description: 'Stepper with 7 steps at step 3',
+  data: {
+    steps: '7',
+    current: '3',
+  },
 };
 
 const meta: StyleComponentProps<'tf-stepper'> = {
@@ -52,7 +67,7 @@ const meta: StyleComponentProps<'tf-stepper'> = {
   tag: 'tf-stepper',
   description: 'A stepper component indicating the current step.',
   component: 'Tourisfair Stepper',
-  variants: [variant1, variant2, variant3, variant4, variant5]
+  variants: [step1, step2, step3, step4, step5, step73],
 };
 
 export const styleTfStepper = (styleBook: StyleBook) => styleBook.addComponent(meta);
