f0d2b5462a978117028a03e69fae51b85f1a3d5a
TopNiz <nizar.ayed@upgrade-code.org>
2023-08-31 10:49:10 +0200

    Corrected single and escaped quotes management

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
       browser: true,
       es2021: true,
    },
+   files: ["src/**/*.ts"],
    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
    overrides: [],
    parser: '@typescript-eslint/parser',
@@ -15,7 +16,7 @@ module.exports = {
    rules: {
       //indent: ['error', 2],
       'linebreak-style': 0,
-      quotes: ['error', 'single'],
+      quotes: ['error', 'single', { "avoidEscape": true }],
       semi: ['error', 'always'],
    },
 };
